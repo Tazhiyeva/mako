8140b86d49db89c100461d404124254cad4b6159
madinatazhiyeva <madinatazhiyeva@gmail.com>
2024-09-04 02:17:32 +0500

    fetch all reviews usting offset_date

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,29 +4,31 @@ import (
 	"fmt"
 	"mako/internal/fetcher"
 	"net/url"
+	"time"
 )
 
 func main() {
+
 	params := url.Values{}
 	params.Add("limit", "50")
-	//params.Add("offset_date", "2023-06-04T15:46:11.636835%2B07:00")
+	params.Add("offset_date", time.Now().Format(time.RFC3339))
 	params.Add("rated", "true")
 	params.Add("sort_by", "date_edited")
 	params.Add("key", "37c04fe6-a560-4549-b459-02309cf643ad")
 	params.Add("locale", "ru_KZ")
 
-	baseURL := "https://public-api.reviews.2gis.com/2.0/branches/9429940000796245/reviews"
+	baseURL := "https://public-api.reviews.2gis.com/2.0/branches/9429940001186378/reviews"
 
 	dgisfetcher := fetcher.NewDGISReviewFetcher(baseURL)
 
-	response, err := dgisfetcher.FetchReview(params)
+	reviews, err := dgisfetcher.FetchReview(params)
 	if err != nil {
 		return
 	}
 
-	for i, r := range response.Reviews {
+	for i, r := range reviews {
 		fmt.Println("Review", (i + 1), ": ", r.User.FirstName, r.User.LastName)
-		fmt.Println("Total count of reviews: ", r.User.ReviewsCount)
+		fmt.Println("Total count of user reviews: ", r.User.ReviewsCount)
 		fmt.Println("----------------------------------------")
 		fmt.Println("Text: ", r.Text)
 		fmt.Println("Rating: ", r.Rating)
@@ -34,5 +36,4 @@ func main() {
 		fmt.Println("Date_created: ", r.DateCreated)
 		fmt.Println()
 	}
-
 }
